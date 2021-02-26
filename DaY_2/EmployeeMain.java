765ebda6a1323849a412aeefa480ac5114713e66
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-02-26 09:20:14 +0530

    Update EmployeeMain.java

--- DaY_2/EmployeeMain.java
+++ DaY_2/EmployeeMain.java
@@ -6,8 +6,8 @@ public class EmployeeMain
 	public static void main(String args[])
 	{
 		Employee obj = new Employee();
-		obj.setName("Sandeep");
-		obj.setAddress("Hyderabad");
+		obj.setName("Subbarao");
+		obj.setAddress("Martur");
 		obj.setMobile("6580964571");
 		System.out.println(obj.getName());
 		System.out.println(obj.getAddress());
