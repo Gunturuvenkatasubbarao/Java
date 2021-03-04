b2a793e847dda8a3df5d6f24ed861b7d04fcb3c6
Gunturuvenkatasubbarao <78855797+Gunturuvenkatasubbarao@users.noreply.github.com>
2021-03-04 22:31:27 +0530

    Create Scorer.java

---
+++ Day5/Scorer.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Scorer {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList <String>list = new ArrayList<String>();
+		ArrayList <String>list1 = new ArrayList<String>();
+		System.out.println("Enter the top 5 scorers of IPL Season 13");
+		for(int i=0;i<5;i++) {
+			list.add(sc.nextLine());
+		}
+		System.out.println("Enter the top 5 scorers of IPL Season 12");
+		for(int i=0;i<5;i++) {
+			list1.add(sc.nextLine());
+		}
+        list.retainAll(list1); 
+        System.out.println("Consistent run scorers");
+        for(String j:list) {
+        	
+			System.out.println(j);
+		}
+	}
+}
